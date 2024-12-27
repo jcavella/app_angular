a274eb1424cf5cadb231792d78a105ac210ec114
Juan Carlos Avella G. <jcavella@gmail.com>
2024-12-27 15:42:04 -0500

    Incluye property binding[]

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -10,10 +10,19 @@ import { RouterOutlet } from '@angular/router';
 })
 export class LabsComponent {
   welcome = 'Hola!';
+  nombre = 'Juan Carlos Avella';
+  disabled = true;
+  img = "https://static.vecteezy.com/system/resources/thumbnails/027/951/137/small/stylish-spectacles-guy-3d-avatar-character-illustrations-png.png";
   //Lista de tareas
   tasks = [
     'Instalar Angular Cli',
     'Crear proyecto',
     'Crear Componentes'
-  ]
+  ];
+
+  //Definir objeto
+  persona ={
+    name:"Juan Carlos Avella G",
+    avatar:"https://static.vecteezy.com/system/resources/thumbnails/027/951/137/small/stylish-spectacles-guy-3d-avatar-character-illustrations-png.png"
+  }
 }
